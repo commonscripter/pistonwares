ed629e8d2ab4279cabc44e6e2a66945c4456b325
commonscripter <216401956+commonscripter@users.noreply.github.com>
2026-08-16 12:13:18 -0400

    Update 6872265039.lua

--- games/6872265039.lua
+++ games/6872265039.lua
@@ -1,7 +1,4 @@
-if not shared.PistonwareAuthenticated then
-	warn('[pistonware] not authenticated -- run the pistonware loader and enter your key')
-	return
-end
+-- AUTHENTICATION CHECK REMOVED - No key required
 
 local run = function(func) func() end
 local cloneref = cloneref or function(obj) return obj end
